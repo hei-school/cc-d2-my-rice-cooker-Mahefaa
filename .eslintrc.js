70e6269e16e2782cbb1f1831d785df36b9204ec2
Mahefaa <hei.mahefa@gmail.com>
2023-12-10 22:10:20 +0300

    chore: lint with ESLint google

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'google'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint']
+};
